c9188b9518c9e548841cee69030acdfb4b02f867
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-10-16 20:15:04 +0300

    Add questions

---
+++ src/main/java/acme/study_guide/chapter01/review_questions/question15/Duck.java
@@ -0,0 +1,8 @@
+package acme.study_guide.chapter01.review_questions.question15;
+
+public class Duck {
+    public void main(String[] args) {
+        for (int i = 1; i <= args.length; i++)
+            System.out.println(args[i]);
+    }
+}
