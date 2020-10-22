a3adf632d77dd2510b35150f61291bb1a9f62efa
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-10-22 20:14:36 +0300

    Add questions

---
+++ src/main/java/acme/study_guide/chapter02/review_questions/question04/Example.java
@@ -0,0 +1,12 @@
+package acme.study_guide.chapter02.review_questions.question04;
+
+public class Example {
+    public static void main(String[] args) {
+//        short numPets = 5L;         // A
+//        int numGrains = 2.0;        // B
+        String name = "Scruffy";    // C
+//        int d = numPets.length();   // D
+//        int e = numGrains.length;   // E
+        int f = name.length();      // F
+    }
+}
---
+++ src/main/java/acme/study_guide/chapter02/review_questions/question18/Example.java
@@ -0,0 +1,15 @@
+package acme.study_guide.chapter02.review_questions.question18;
+
+public class Example {
+    public static void main(String[] args) {
+        var squirrel = new Object();
+        int capybara = 2, mouse, beaver = -1;
+//        char chipmunk = -1;
+        squirrel = "";
+        beaver = capybara;
+        System.out.println(capybara);
+//        System.out.println(mouse);
+        System.out.println(beaver);
+//        System.out.println(chipmunk);
+    }
+}
