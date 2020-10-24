2bd67e800aef2c323414604be5878e0c5146e5c4
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-10-24 20:51:25 +0300

    Add questions

---
+++ src/main/java/acme/study_guide/chapter03/numeric_promotion_rules/Example.java
@@ -0,0 +1,9 @@
+package acme.study_guide.chapter03.numeric_promotion_rules;
+
+public class Example {
+    public static void main(String[] args) {
+        int x = 1;
+        long y = 33;
+        var z = x * y;
+    }
+}
