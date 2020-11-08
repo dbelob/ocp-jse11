dc85210dcbe2f73ff6ec7192ed5e70d819ea338b
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2020-11-08 21:06:45 +0300

    Add test examples

---
+++ src/main/java/acme/study_guide/chapter05/creating_an_array_of_primitives/Example.java
@@ -0,0 +1,9 @@
+package acme.study_guide.chapter05.creating_an_array_of_primitives;
+
+public class Example {
+    public static void main(String[] args) {
+        int[] numbers1 = new int[3];
+        int[] numbers2 = new int[] {42, 55, 99};
+        int[] numbers3 = {42, 55, 99};
+    }
+}
