a82299450bddc0b6defa66f7791220a6fd64b968
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-04-20 20:32:21 +0300

    Add questions

---
+++ chapters/src/main/java/acme/study_guide/chapter19/review_questions/question03/Eagle.java
@@ -0,0 +1,34 @@
+package acme.study_guide.chapter19.review_questions.question03;
+
+import java.io.Serializable;
+
+class Bird {
+    protected transient String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Bird() {
+        this.name = "Matt";
+    }
+}
+
+public class Eagle extends Bird implements Serializable {
+    {
+        this.name = "Olivia";
+    }
+
+    public Eagle() {
+        this.name = "Bridget";
+    }
+
+    public static void main(String[] args) {
+        var e = new Eagle();
+        e.name = "Adeline";
+    }
+}
