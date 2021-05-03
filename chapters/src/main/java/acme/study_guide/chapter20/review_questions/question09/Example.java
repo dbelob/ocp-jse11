d7bd912075cec57734322767a23872cc79682814
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-05-03 23:35:06 +0300

    Add questions

--- chapters/src/main/java/acme/study_guide/chapter20/review_questions/question09/Example.java
+++ chapters/src/main/java/acme/study_guide/chapter20/review_questions/question09/Example.java
@@ -8,7 +8,7 @@ public class Example {
         var path1 = Path.of("/pets/../cat.txt");
         var path2 = Paths.get("./dog.txt");
 
-        System.out.println(path1.resolve(path2));   // /pets/../cat.txt./dog.txt
+        System.out.println(path1.resolve(path2));   // /pets/../cat.txt/./dog.txt
         System.out.println(path2.resolve(path1));   // /pets/../cat.txt
     }
 }
