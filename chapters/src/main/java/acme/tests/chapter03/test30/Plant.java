77681b2f5c500be6ca44f9b45d62b73656e6381b
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-06-05 21:30:33 +0300

    Add test examples

---
+++ chapters/src/main/java/acme/tests/chapter03/test30/Plant.java
@@ -0,0 +1,17 @@
+package acme.tests.chapter03.test30;
+
+class Bush extends Plant {
+    String type = "bush";
+}
+
+public class Plant {
+    String type = "plant";
+
+    public static void main(String[] args) {
+        Plant w1 = new Bush();
+        Bush w2 = new Bush();
+        Plant w3 = w2;
+
+        System.out.print(w1.type + "," + w2.type + "," + w3.type);
+    }
+}
