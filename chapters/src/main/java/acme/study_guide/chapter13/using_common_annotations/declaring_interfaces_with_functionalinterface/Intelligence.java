a9c3d7f73c57986fe41d9215dcd6855e47ce037f
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-08-09 20:21:11 +0300

    Add test examples

---
+++ chapters/src/main/java/acme/study_guide/chapter13/using_common_annotations/declaring_interfaces_with_functionalinterface/Intelligence.java
@@ -0,0 +1,6 @@
+package acme.study_guide.chapter13.using_common_annotations.declaring_interfaces_with_functionalinterface;
+
+@FunctionalInterface
+public interface Intelligence {
+    int cunning();
+}
