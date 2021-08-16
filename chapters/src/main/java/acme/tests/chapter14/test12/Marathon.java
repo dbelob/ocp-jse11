7840cc6baa55a05f393b9fe9cb326389d32d3deb
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-08-16 20:53:19 +0300

    Add test examples

---
+++ chapters/src/main/java/acme/tests/chapter14/test12/Marathon.java
@@ -0,0 +1,29 @@
+package acme.tests.chapter14.test12;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+class Runner {
+    private int numberMinutes;
+
+    public Runner(int n) {
+        numberMinutes = n;
+    }
+
+    public int getNumberMinutes() {
+        return numberMinutes;
+    }
+}
+
+public class Marathon {
+    public static void main(String[] args) {
+        var match = Optional.ofNullable(161);    // line z
+
+        var runners = Stream.of(new Runner(183),
+                new Runner(161), new Runner(201));
+        var opt = runners.map(Runner::getNumberMinutes)
+                .filter(m -> match.get().equals(m))
+                .peek(System.out::println)
+                .count();
+    }
+}
